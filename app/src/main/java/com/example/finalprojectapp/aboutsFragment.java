8f7d939ca76daab95652f44ed39b444e69812673
mahfuzur <mdmahfuazur7788@gamil.com>
2022-09-30 04:16:26 +0600

    aboutsFragment

---
+++ app/src/main/java/com/example/finalprojectapp/aboutsFragment.java
@@ -0,0 +1,62 @@
+package com.example.finalprojectapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+
+import  androidx.fragment.app.Fragment;
+
+
+public class aboutsFragment extends  Fragment{
+
+
+    public  aboutsFragment(){
+
+    }
+
+
+    public  View onCreatView(LayoutInflater inflater, ViewGroup Container, Bundle savedInstanceState){
+      getActivity().setTitle("Shopify-Cart");
+      return  inflater.inflate(R.layout.fragment_abouts, Container, false);
+
+    }
+
+
+
+    public static  class CatListFragment extends  Fragment{
+
+        private  static final  String ARG_PARAM1 = "param1";
+        private  static final  String ARG_PARAM2 = "param2";
+
+        private  String mParam1;
+        private  String mParam2;
+
+        public CatListFragment() {
+
+        }
+
+        public  static  CatListFragment newInstance (String Param1, String Param2){
+            CatListFragment fragment = new CatListFragment();
+            Bundle args = new Bundle();
+            args.putString(ARG_PARAM1, Param1);
+            args.putString(ARG_PARAM2, Param2);
+            fragment.setArguments(args);
+            return  fragment;
+        }
+
+        public void  onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            if (getArguments()!=null){
+                mParam1 = getArguments().getString((ARG_PARAM1));
+                mParam2 = getArguments().getString((ARG_PARAM2));
+            }
+        }
+
+        public  View OnCreateView(LayoutInflater inflater, ViewGroup Container, Bundle savedInstanceState){
+            return  inflater.inflate(R.layout.fragment_cartlist, Container, false);
+        }
+
+
+    }
+}
