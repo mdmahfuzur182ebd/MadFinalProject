3d9d62d062de7f1d87f8c822ad0a0839b4d2b267
mahfuzur <mdmahfuazur7788@gamil.com>
2022-09-30 11:54:02 +0600

    themes are use but some problem

--- app/src/main/java/com/example/finalprojectapp/HomeActivity.java
+++ app/src/main/java/com/example/finalprojectapp/HomeActivity.java
@@ -16,6 +16,29 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+public class MainActivity extends AppCompatActivity {
+    private EditText display;
+    Switch switch_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        switch_id = findViewById(R.id.switch_id);
+
+        switch_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (switch_id.isChecked())
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+    }
+}
+
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout dlayout;
--- app/src/main/java/com/example/finalprojectapp/MainActivity.java
+++ app/src/main/java/com/example/finalprojectapp/MainActivity.java
@@ -5,10 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText display;
+    Switch switch_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switch_id = findViewById(R.id.switch_id);
+
+        switch_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (switch_id.isChecked())
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
     }
 }
+
+
+
+
