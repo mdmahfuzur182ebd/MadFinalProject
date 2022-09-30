a6818f2df4d83db750e84debe64d507b3c08379c
mahfuzur <mdmahfuazur7788@gamil.com>
2022-09-30 11:17:29 +0600

    user SQLITEdatabase done

--- app/src/main/java/com/example/finalprojectapp/MediMartDbHelper.java
+++ app/src/main/java/com/example/finalprojectapp/MediMartDbHelper.java
@@ -1,12 +1,61 @@
 package com.example.finalprojectapp;
 
+import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class MediMartDbHelper {
-    public MediMartDbHelper(Context context) {
+import androidx.annotation.Nullable;
+
+public class MediMartDbHelper extends SQLiteOpenHelper {
+   public  MediMartDbHelper(@Nullable Context context){
+       super(context, "medimart.db", null, 1);
+   }
+
+   public void  onCreate(SQLiteDatabase db){
+       db.execSQL("CREATE TABLE IF NOT EXISTS user(_id integer primary key autoincrement,"+
+               "userid varchar(100), pwd varchar(30), role varchar(20))");
+
+       db.execSQL("CREATE TABLE IF NOT EXISTS customers(_id integer primary key autoincrement,"+
+               "userid varchar(100), city varchar(100), gender varchar(20)," + "email varchar(100), phone varchar(20))");
+
+       db.execSQL("CREATE TABLE IF NOT EXISTS cart(_id integer primary key autoincrement,"+
+               "userid varchar(100), product varchar(100), role varchar(20),qty int)");
+
+       db.execSQL("CREATE TABLE IF NOT EXISTS orders(_id int primary key, "+
+               "userid varchar(100), orderdate date, status varchar(20), total int)");
+
+       db.execSQL("INSERT INTO users(userid,pwd,role)"+ "VALUES ('admin', 'admin', 'admin')");
+   }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
     }
 
-    public void addCustomer(String uname, String city, String gender, String email, String phone, String pwd) {
+    public void addCustomer(String cname, String city, String gender, String email, String phone, String pwd){
+       SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("cname", cname);
+        cv.put("city", city);
+        cv.put("gender", gender);
+        cv.put("email", email);
+        cv.put("phone ", phone);
+        db.insert("customers", null, cv);
+        ContentValues cv2 = new ContentValues();
+        cv2.put("userid", phone);
+        cv2.put("pwd", pwd);
+        cv2.put("role", "customer");
+        db.insert("users", null,cv2);
     }
+
+    public  Cursor getCustomer (String userid){
+       SQLiteDatabase db = getReadableDatabase();
+       Cursor c =db.rawQuery("SELECT * FROM customers WHERE phone=?", new String[]{userid});
+       return c;
+    }
+
+
 }
---
+++ app/src/main/java/com/example/finalprojectapp/MediMartUtils.java
@@ -0,0 +1,14 @@
+package com.example.finalprojectapp;
+
+public class MediMartUtils {
+    public static void toTitleCase(String str) {
+//        if (str == null){
+//            return null;
+//        }
+//
+//        boolean space = true;
+//        StringBuilder builder = new StringBuilder(str);
+//    }
+
+    }
+}
